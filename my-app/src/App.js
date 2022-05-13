6c491757c64b5adb2b223a855a5b1360c1c910f5
Mina <minasoha@yahoo.com>
2022-05-12 22:15:28 -0400

    wip

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,36 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
 
-import './App.css';
+import FormControl from "react-bootstrap/FormControl";
+import InputGroup from "react-bootstrap/InputGroup";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <>
-    <h1>Fun with AI</h1>
-    <form>
-      <label>Enter prompt
-        <input type="text" />
-      </label>
-      <button>Submit</button>
-    </form>
-    <h2>Responses</h2>
-    <div classname="responses">
-      <h5>Prompt:</h5>
-      <h5>Response: </h5>
-    </div>
-    </>
-  );
+ const [facts, setFacts] = useState({});
+ const onSubmit = (e) => {
+  e.preventdefault();
+ };
+//fetch the facts from https://catfact.ninja/fact
+ return (
+  <>
+   <h1>Fun with AI</h1>
+   <InputGroup>
+    <InputGroup.Text>With textarea</InputGroup.Text>
+    <FormControl as='textarea' aria-label='With textarea' />
+   </InputGroup>
+   <Button onSubmit={onSubmit}>Submit</Button>
+   <h2>Responses</h2>
+   <div className='responses'>
+    <h5>Prompt:</h5>
+    <h5>Response: </h5>
+    {facts.map((fact) => {
+     return <pre>{JSON.stringify(fact)}</pre>;
+    })}
+   </div>
+  </>
+ );
 }
 
 export default App;
